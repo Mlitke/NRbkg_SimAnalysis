5677985f58609752fc3e60642598c4d52d8b020b
lkorley <lukekorley@gmail.com>
2018-09-24 14:51:58 -0400

    Including ML's stacking

---
+++ runmerge.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+MYDIR=/global/homes/l/lkorley/myprojectdir/HighNR
+INDIR=${MYDIR}/output
+source /cvmfs/lz.opensciencegrid.org/BACCARAT/release-3.14.3/setup.sh
+FileList=""
+cd ${INDIR}
+for f in *
+do
+	infile=${INDIR}/${f}/Plots/*_Plots.root
+	if ls ${infile} 1> /dev/null 2>&1; then
+		echo "${f} is a good boy"
+	else
+		echo "${f} is a naughty one"
+		continue
+	fi
+
+	if [ "${FileList}" != "" ]
+	then
+		FileList=${FileList},${infile}
+	else
+		FileList=${infile}
+	fi
+done
+
+sbatch --job-name=merge --output=${MYDIR}/sh_out/merge.%j.out --error=${MYDIR}/sh_out/merge.%j.err ${MYDIR}/NRint.slr ${MYDIR}/src/plotrates.py "MergePlot" "${FileList}" "${INDIR}"
